ff944b84bf3ad8573a9ff8c19b69426e4a878186
Jody Phelan <ipmbjphe@macb0095.home>
2017-12-21 16:42:24 +0000

    documentation

---
+++ bam_report.py
@@ -0,0 +1,67 @@
+import pathogenseq as ps
+import argparse
+import json
+
+def main(args):
+    if not args.bam:
+        print "Please provide bam file"
+        quit()
+    else:
+         ps.filecheck(args.bam)
+    if not args.ref:
+        print "Please provide reference"
+        quit()
+    else:
+        ps.filecheck(args.ref)
+        fasta = ps.fasta(args.ref)
+    if not args.prefix:
+        print "Please provide prefix"
+        quit()
+    if args.gff and args.bed:
+        print "Please provide either a GFF file or BED file but not both...Exiting!"
+        quit()
+    if args.gff:
+        if not args.gffkey:
+            print "Please provide the key to look for in the GFF file...Exiting!"
+            quit()
+        else:
+            ps.filecheck(args.gff)
+    if args.bed: ps.filecheck(args.bed)
+
+    cov_json = "%s.cov.json" % args.prefix
+    stats_json = "%s.bam_stats.json" % args.prefix
+    region_json = "%s.regionscov.json" % args.prefix
+
+    stats = {}
+
+    bamqc = ps.qc_bam(args.bam,args.ref)
+    for s in fasta.fa_dict:
+        cov_plot = "%s.%s.cov.png" % (args.prefix,s)
+        bamqc.plot_cov(s,cov_plot)
+    bamqc.save_cov(cov_json)
+    stats["pct_reads_mapped"] = bamqc.pct_reads_mapped
+    stats["med_dp"] = bamqc.med_dp
+    region_cov = {}
+    if args.gff:
+        region_cov = bamqc.gff_cov(args.gff,args.gffkey)
+    elif args.bed:
+        region_cov = bamqc.bed_cov(args.bed)
+    json.dump(stats,open(stats_json,"w"))
+    json.dump(region_cov,open(region_json,"w"))
+
+
+
+
+
+parser = argparse.ArgumentParser(description='TBProfiler pipeline',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--bam','-a', help='BAM file')
+parser.add_argument('--ref','-r', help='Reference Sequence')
+parser.add_argument('--prefix','-p', help='Prefix for files')
+parser.add_argument('--gff','-g', help='GFF file')
+parser.add_argument('--gffkey','-k', help='GFF key')
+parser.add_argument('--bed','-b', help='BED file')
+
+parser.set_defaults(func=main)
+
+args = parser.parse_args()
+args.func(args)
---
+++ fastq_report.py
@@ -0,0 +1,34 @@
+import pathogenseq as ps
+import argparse
+import json
+
+def main(args):
+    if not args.r1:
+        print "Please provide at least one fastq file with -1...Exiting"
+        quit()
+    else:
+        ps.filecheck(args.r1)
+    if args.r2: ps.filecheck(args.r2)
+    if not args.prefix:
+        print "Please provide a file output prefix...Exiting"
+        quit()
+
+    stats = {}
+    fastqqc = ps.qc_fastq(args.prefix,args.r1,args.r2) if args.r2 else ps.qc_fastq(args.prefix,args.r1)
+    stats["mean_read_len"] = fastqqc.mean_read_len
+    stats["read_num"] = fastqqc.read_num
+
+    stats_json = "%s.fastq_stats.json" % args.prefix
+    json.dump(stats,open(stats_json,"w"))
+
+
+parser = argparse.ArgumentParser(description='TBProfiler pipeline',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--r1','-1', help='First read file')
+parser.add_argument('--r2','-2', help='Second read file')
+parser.add_argument('--prefix','-p', help='Prefix for files')
+parser.add_argument('--kraken', help='Use kraken files')
+
+parser.set_defaults(func=main)
+
+args = parser.parse_args()
+args.func(args)
