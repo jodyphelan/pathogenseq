1a33f324e1b754b134fc82a77af01494411e612b
Jody Phelan <ipmbjphe@macb0095.local>
2018-01-02 14:06:30 +0000

    examl script

--- scripts/collate_json.py
+++ scripts/collate_json.py
@@ -14,4 +14,4 @@ for s in samples:
 
 print "\t".join(keys)
 for s in samples:
-	print "\t".join([str(results[s][k]) if k in results[s] else "-" for k in keys])
+	print "%s\t%s" % ("\t".join([str(results[s][k]) if k in results[s] else "-" for k in keys]))
