27f89d75cc94ff1cd437b007d8787c202b3da71b
Jody Phelan <jody.phelan@lshtm.ac.uk>
2018-01-11 19:44:51 +0000

    scripts/minION_pipeline.py

--- pathogenseq/files.py
+++ pathogenseq/files.py
@@ -28,7 +28,7 @@ def bwa_index(ref):
 	cmd = "bwa index %s" % ref
 	run_cmd(cmd)
 
-def run_cmd(cmd,verbose=True):
+def run_cmd(cmd,verbose=2):
 	"""
 	Wrapper to run a command using subprocess with 3 levels of verbosity and automatic exiting if command failed
 	"""
