974d150e67c6c1b7fdd707f57bff44e8f0c3b6e8
Jody Phelan <jody.phelan@lshtm.ac.uk>
2018-01-12 00:29:49 +0000

    vcf2fasta

--- pathogenseq/mvcf.py
+++ pathogenseq/mvcf.py
@@ -1,5 +1,7 @@
 import sys
 from files import *
+from fasta import *
+
 v = True
 class bcf:
 	params = {}
@@ -29,8 +31,9 @@ class bcf:
 		O = open(self.params["matrix_file"],"w").write("chr\tpos\tref\t%s\n" % ("\t".join(self.samples)))
 		cmd = "bcftools query -f '%%CHROM\\t%%POS\\t%%REF[\\t%%IUPACGT]\\n' %(bcf)s | sed 's/\.\/\./N/g' >> %(matrix_file)s" % self.params
 		run_cmd(cmd,verbose=v)
-	def vcf_to_fasta(self,filename):
+	def vcf_to_fasta(self,filename,threads=20):
 		"""Create a fasta file from the SNPs"""
+		self.params["threads"] = threads
 		cmd = "bcftools query -l %(bcf)s | parallel -j %(threads)s \"(printf '>'{}'\\n' > {}.fa; bcftools query -s {} -f '[%%IUPACGT]' %(bcf)s >> {}.fa; printf '\\n' >> {}.fa)\"" % self.params
 		run_cmd(cmd)
 		O = open(filename,"w")
