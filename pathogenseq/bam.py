a800e889a5627a5fafb191307954d540e57d4473
Jody Phelan <jody.phelan@lshtm.ac.uk>
2018-03-12 18:34:36 +0000

    mixed as missing in csq

--- pathogenseq/bam.py
+++ pathogenseq/bam.py
@@ -81,7 +81,7 @@ class bam:
 		cmd = "rm `%(cmd_split_chr)s  | awk '{print \"%(prefix)s_\"$1\".bcf*\"}'`" % self.params
 		run_cmd(cmd)
 
-		return bcf(final_bcf,prefix=self.prefix)
+		return bcf(self.params["gbcf_file"],prefix=self.prefix)
 
 	def call_variants(self,gff_file=None,bed_file=None,call_method="optimise",min_dp=10,threads=4,mixed_as_missing=False):
 		self.params["min_dp"] = min_dp
