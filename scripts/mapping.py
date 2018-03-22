877322b70d998c0c364b673143425d4462e14315
Jody Phelan <jody.phelan@lshtm.ac.uk>
2018-03-22 15:12:15 +0000

    bowtie default mapper

--- scripts/mapping.py
+++ scripts/mapping.py
@@ -3,6 +3,9 @@ import pathogenseq
 import argparse
 
 def main(args):
+	if not args.prefix:
+		print "Please specify prefix with -p"
+		quit(1)
 	x= pathogenseq.fastq(args.prefix,args.ref,args.r1,args.r2,threads=args.threads)
 	x.illumina(mapper=args.mapper)
 
@@ -12,7 +15,7 @@ parser.add_argument('--r2','-2', help='Second read file')
 parser.add_argument('--ref','-r', help='Reference Sequence')
 parser.add_argument('--threads','-t', type=int, default=1, help='Number of threads')
 parser.add_argument('--prefix','-p', help='Prefix for files')
-parser.add_argument('--mapper','-m', type=str,choices=["bwa","minimap2","bowtie2"],help='Prefix for files')
+parser.add_argument('--mapper','-m', type=str,default="minimap2",choices=["bwa","minimap2","bowtie2"],help='Mapping tool to use')
 parser.set_defaults(func=main)
 
 args = parser.parse_args()
