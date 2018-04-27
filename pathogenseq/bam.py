c7d4f11758145c93ac1e083fbdf2f0a2f49de3df
Jody Phelan <jody.phelan@lshtm.ac.uk>
2018-04-27 16:25:09 +0100

    minor bugfix

--- pathogenseq/bam.py
+++ pathogenseq/bam.py
@@ -65,7 +65,7 @@ class bam:
 			if read_num==0:
 				cmd = "bcftools mpileup  -f %(ref_file)s %(bam_file)s %(mpileup_options)s -r %(tmp)s | bcftools call %(vtype)s -m | bcftools +setGT -Ob -o %(prefix)s.%(tmp)s.bcf -- -t a -n ." % self.params
 			else:
-				cmd = "samtools index %(prefix)s.%(tmp)s.bam && bcftools mpileup  -f %(ref_file)s %(prefix)s.%(tmp)s.bam %(mpileup_options)s -r %(tmp)s | bcftools call -t %(tmp)s %(vtype)s -mg %(min_dp)s | bcftools norm -f %(ref_file)s  | bcftools +setGT -Ob -o %(prefix)s.%(tmp)s.bcf -- -t q -i 'FMT/DP<%(min_dp)s' -n .\"" % self.params
+				cmd = "samtools index %(prefix)s.%(tmp)s.bam && bcftools mpileup  -f %(ref_file)s %(prefix)s.%(tmp)s.bam %(mpileup_options)s -r %(tmp)s | bcftools call -t %(tmp)s %(vtype)s -mg %(min_dp)s | bcftools norm -f %(ref_file)s  | bcftools +setGT -Ob -o %(prefix)s.%(tmp)s.bcf -- -t q -i 'FMT/DP<%(min_dp)s' -n ." % self.params
 			run_cmd(cmd)
 	def get_calling_params(self):
 		dp = []
