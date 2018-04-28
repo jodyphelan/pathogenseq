7b430ed2146c9e363b1c1ce0f3e70533d50a4fd5
Jody Phelan <jody.phelan@lshtm.ac.uk>
2018-04-28 13:08:00 +0100

    multithreads primer reconstruction

--- pathogenseq/qc.py
+++ pathogenseq/qc.py
@@ -111,6 +111,7 @@ class qc_fastq:
 		self.params["centrifuge_report"] = "%(prefix)s.centrifuge.report.txt" % self.params
 		self.params["centrifuge_log"] = "%(prefix)s.centrifuge.log" % self.params
 		self.params["threads"] = threads
+		print self.paired
 		if self.paired:
 			cmd = "centrifuge -x %(centrifuge_db)s -1 %(fq1)s -2 %(fq2)s -S %(centrifuge_log)s --report-file %(centrifuge_report)s -p %(threads)s" % self.params
 		else:
