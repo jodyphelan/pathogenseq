4fe7498b46c7e29440590cd0b899fb7d0cfad6cf
Jody Phelan <jody.phelan@lshtm.ac.uk>
2018-04-29 19:21:18 +0100

    multithreads primer reconstruction

--- scripts/bam2vcf.py
+++ scripts/bam2vcf.py
@@ -4,13 +4,14 @@ import pathogenseq as ps
 import argparse
 
 def main(args):
-	bam = ps.bam(bam_file=args.bam,ref_file=args.ref,prefix=args.prefix,threads=args.threads)
+	bam = ps.bam(bam_file=args.bam,ref_file=args.ref,prefix=args.prefix,threads=args.threads,vtype=args.vtype)
 	bam.gbcf(threads=args.threads,platform=args.platform,primers=args.primers,call_method=args.method)
 
 parser = argparse.ArgumentParser(description='TBProfiler pipeline',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('bam', help='First read file')
 parser.add_argument('ref', help='Second read file')
 parser.add_argument('prefix', help='Reference Sequence')
+parser.add_argument('--vtype','-v', type=str, default="snps", choices=["snps","indels","both"],help='Number of threads')
 parser.add_argument('--threads','-t', type=int, default=1, help='Number of threads')
 parser.add_argument('--platform','-p', type=str,default="illumina",choices=["illumina","minION"],help='Mapping tool to use')
 parser.add_argument('--primers', type=str,default=None,help='Mapping tool to use')
