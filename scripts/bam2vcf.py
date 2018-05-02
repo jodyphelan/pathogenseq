d1b70abe3a8345e16491da9e98777a56ab496301
Jody Phelan <jody.phelan@lshmt.ac.uk>
2018-05-02 17:48:45 +0100

    merge rewrite

--- scripts/bam2vcf.py
+++ scripts/bam2vcf.py
@@ -5,7 +5,7 @@ import argparse
 
 def main(args):
 	bam = ps.bam(bam_file=args.bam,ref_file=args.ref,prefix=args.prefix,threads=args.threads)
-	bam.gbcf(threads=args.threads,platform=args.platform,primers=args.primers,call_method=args.method,vtype=args.vtype)
+	bam.gbcf(threads=args.threads,platform=args.platform,primers=args.primers,call_method=args.method,vtype=args.vtype,min_dp=args.min_dp)
 
 parser = argparse.ArgumentParser(description='TBProfiler pipeline',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('bam', help='First read file')
@@ -16,6 +16,7 @@ parser.add_argument('--threads','-t', type=int, default=1, help='Number of threa
 parser.add_argument('--platform','-p', type=str,default="illumina",choices=["illumina","minION"],help='Mapping tool to use')
 parser.add_argument('--primers', type=str,default=None,help='Mapping tool to use')
 parser.add_argument('--method', type=str,default="optimise",choices=["optimise","high","low"],help='Mapping tool to use')
+parser.add_argument('--min_dp', type=int,default=10,help='Minimum depth required to call an allele')
 
 parser.set_defaults(func=main)
 
