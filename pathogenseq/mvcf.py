44eb5cea7d403372ec2c4af4a811d8abedebbdcd
Jody Phelan <jody.phelan@lshmt.ac.uk>
2018-05-02 17:09:23 +0100

    merge rewrite

--- pathogenseq/mvcf.py
+++ pathogenseq/mvcf.py
@@ -735,7 +735,7 @@ DATA
 		add_arguments_to_self(self,locals())
 		for s in samples:
 			self.tmp_sample = s
-			cmd = "bcftools view -s %(tmp_sample)s -i 'GT=="./."' %(filename)s | bcftools query -f '%%CHROM\\t%%POS'" % vars(self)
+			cmd = "bcftools view -s %(tmp_sample)s -i 'GT==\"./.\"' %(filename)s | bcftools query -f '%%CHROM\\t%%POS'" % vars(self)
 			self.tmp_file = "%s.missing.bed" % vars(self)
 			TMP = open(self.tmp_file,"w")
 			for l in cmd_out(cmd):
