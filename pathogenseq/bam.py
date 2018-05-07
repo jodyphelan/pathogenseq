d4f9b7bea69530f597fc65e26a8731215154f05c
Jody Phelan <jody.phelan@lshtm.ac.uk>
2018-05-07 10:18:18 +0100

    primer search

--- pathogenseq/bam.py
+++ pathogenseq/bam.py
@@ -63,8 +63,7 @@ class bam:
 			tmp_bam = "%s.%s.bam" % (self.prefix,pid)
 			self.params["tmp"] = "%s:%s-%s" % (chrom,start,end)
 			self.params["pid"] = pid
-			log("Extracting reads for %s" % pid)
-			log("%s:%s-%s")
+			log("Extracting reads for %s" % pid)#
 			read_num = get_overlapping_reads(self.bam,chrom,start,end,tmp_bam,flank=30,threads=threads)
 			if read_num==0:
 				#cmd = "bcftools mpileup  -f %(ref_file)s %(bam_file)s %(mpileup_options)s -r %(tmp)s | bcftools call %(vtype)s -m | bcftools +setGT -Ob -o %(prefix)s.%(pid)s.bcf -- -t a -n ." % self.params
@@ -171,7 +170,7 @@ class bam:
 		cmd = "rm `%(cmd_split_chr)s  | awk '{print \"%(prefix)s_\"$2\".bcf*\"}'`" % self.params
 		run_cmd(cmd)
 		if primers:
-			rm_files([self.params["non_primer_bcf"],self.params["primer_bcf"]])#),self.params["primer_bed_file"]])
+			rm_files([self.params["primer_bed_file"],self.params["non_primer_bcf"],self.params["primer_bcf"]])
 
 		return bcf(self.params["bcf_file"],prefix=self.prefix)
 
