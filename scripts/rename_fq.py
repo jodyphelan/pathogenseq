293b829de9ff94878346208c140af24a1e3b1bf4
Jody Phelan <jody.phelan@lshmt.ac.uk>
2018-05-15 15:54:43 +0100

    csv2fasta

--- scripts/rename_fq.py
+++ scripts/rename_fq.py
@@ -4,7 +4,7 @@ import pathogenseq.files as psf
 infile = sys.argv[1]
 
 for row in csv.DictReader(open(infile)):
-	f1 = "%s.fastq.gz"
-	f2 = "%s.fastq.gz"
+	f1 = "%s.fastq.gz" % row["Barcode"]
+	f2 = "%s.%s.fastq.gz" % (row["Name"],row["Barcode"])
 	if psf.filecheck(f1,True)
 	os.rename(f1,f2)
