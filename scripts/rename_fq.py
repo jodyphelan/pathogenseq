ef094a689ea0082b5a66554280f9ce9d43738a3a
Jody Phelan <jody.phelan@lshmt.ac.uk>
2018-05-15 16:31:55 +0100

    csv2fasta

--- scripts/rename_fq.py
+++ scripts/rename_fq.py
@@ -7,5 +7,5 @@ infile = sys.argv[1]
 for row in csv.DictReader(open(infile)):
 	f1 = "%s.fastq.gz" % row["Barcode"]
 	f2 = "%s.%s.fastq.gz" % (row["Name"],row["Barcode"])
-	if psf.filecheck(f1,True)
+	psf.filecheck(f1,True)
 	os.rename(f1,f2)
