3d620adccb7442757841bd047ecbef00cab7745b
Jody Phelan <jody.phelan@lshmt.ac.uk>
2018-05-21 10:17:45 +0100

    transfer

--- pathogenseq/mvcf.py
+++ pathogenseq/mvcf.py
@@ -483,10 +483,13 @@ dev.off()
 				if info[-1]=="pseudogene": continue
 				gene = info[1]
 				if info[0]=="intron":continue
+				if info[0]=="coding_sequence":
+					cng = "%s%s>%s" % (ann_pos,ref,alt)
+					variants[sample].append({"sample":sample,"gene_id":ann_gene,"chr":chrom,"genome_pos":pos,"type":"non_coding","change":cng,"freq":adr[call2]})
 				if info[0]=="frameshift&start_lost" or info[0]=="missense&inframe_altering" or info[0]=="missense" or info[0]=="*missense" or info[0]=="start_lost" or info[0]=="*start_lost" or info[0]=="*stop_lost" or info[0]=="stop_lost" or info[0]=="stop_gained" or info[0]=="*stop_gained":
 					variants[sample].append({"sample":sample,"gene_id":gene,"chr":chrom,"genome_pos":pos,"type":info[0],"change":info[5],"freq":adr[call2]})
 
-				elif info[0]=="coding_sequence" or info[0]=="stop_lost&frameshift" or info[0]=="inframe_insertion" or info[0]=="*inframe_insertion" or info[0]=="inframe_deletion" or info[0]=="*inframe_deletion" or info[0]=="frameshift" or info[0]=="*frameshift" or info[0]=="synonymous" or info[0]=="*synonymous" or info[0]=="stop_retained":
+				elif info[0]=="stop_lost&frameshift" or info[0]=="inframe_insertion" or info[0]=="*inframe_insertion" or info[0]=="inframe_deletion" or info[0]=="*inframe_deletion" or info[0]=="frameshift" or info[0]=="*frameshift" or info[0]=="synonymous" or info[0]=="*synonymous" or info[0]=="stop_retained":
 					change_num,ref_nuc,alt_nuc =  parse_mutation(info[6])
 					change = "%s%s>%s" % (ann_pos,ref_nuc,alt_nuc) if ann_pos else None
 					variants[sample].append({"sample":sample,"gene_id":gene,"chr":chrom,"genome_pos":pos,"type":info[0],"change":change,"freq":adr[call2]})
