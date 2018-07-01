2abc6bf770dd96f50ff10be8bb2fab83a1234e11
jody <jody@jodys-MacBook-Pro-2.local>
2018-07-01 23:32:45 +0100

    max cov on gbcf

--- scripts/bam2vcf.py
+++ scripts/bam2vcf.py
@@ -4,8 +4,13 @@ import pathogenseq as ps
 import argparse
 
 def main(args):
+	if args.high_cov_as_missing:
+		bamqc = bam.get_bam_qc()
+		max_dp = bamqc.med_dp*3
+	else:
+		max_dp = None
 	bam = ps.bam(bam_file=args.bam,ref_file=args.ref,prefix=args.prefix,threads=args.threads)
-	bam.gbcf(threads=args.threads,platform=args.platform,primers=args.primers,call_method=args.method,vtype=args.vtype,min_dp=args.min_dp,mpileup_options=args.mpileup_options)
+	bam.gbcf(threads=args.threads,max_dp=max_dp,platform=args.platform,primers=args.primers,call_method=args.method,vtype=args.vtype,min_dp=args.min_dp,mpileup_options=args.mpileup_options)
 
 parser = argparse.ArgumentParser(description='TBProfiler pipeline',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('bam', help='First read file')
@@ -18,6 +23,7 @@ parser.add_argument('--primers', type=str,default=None,help='Mapping tool to use
 parser.add_argument('--method', type=str,default="optimise",choices=["optimise","high","low"],help='Mapping tool to use')
 parser.add_argument('--min_dp', type=int,default=10,help='Minimum depth required to call an allele')
 parser.add_argument('--mpileup_options',default=None,type=str, help='First read file')
+parser.add_argument('--high_cov_as_missing', action='store_true')
 parser.set_defaults(func=main)
 
 args = parser.parse_args()
