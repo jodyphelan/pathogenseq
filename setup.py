ecc2526ad92b9d667d35ccea13b6318c1099d410
jody <jody@jodys-MacBook-Pro-2.local>
2018-07-15 10:35:44 +0100

    remove redundant files

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setuptools.setup(
 	packages=["pathogenseq",],
 	license="MIT",
 	long_description="Pathogenseq variant calling pipeline",
+	install_requires=['numpy','pyvcf','tqdm','ete3','biopython','colour'],
 	scripts=
 		['scripts/bcf2consensus.py',
 		 'scripts/bam_report.py',
