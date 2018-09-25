431c04765595fea64872c4c652e720096d9c0fd3
Jody Phelan <jody.phelan@lshmt.ac.uk>
2018-09-25 15:45:59 +0100

    added argparse

--- scripts/bcf2consensus.py
+++ scripts/bcf2consensus.py
@@ -2,6 +2,15 @@
 import sys
 import pathogenseq as ps
 
-bcf_file,ref_file = sys.argv[1:]
-bcf = ps.bcf(bcf_file)
-bcf.generate_consensus(ref_file)
+def main(args):
+	bcf_file,ref_file = sys.argv[1:]
+	bcf = ps.bcf(bcf_file)
+	bcf.generate_consensus(ref_file)
+
+parser = argparse.ArgumentParser(description='TBProfiler pipeline',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('bcf',help='bcf file')
+parser.add_argument('ref',help='reference file')
+parser.set_defaults(func=main)
+
+args = parser.parse_args()
+args.func(args)
