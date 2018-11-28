7741d5b9620f98193d93539dfa22282ed2fa740f
jodyphelan <jody.phelan@lshtm.ac.uk>
2018-11-28 20:44:35 +0000

    test mp

--- scripts/bcf2consensus.py
+++ scripts/bcf2consensus.py
@@ -7,11 +7,12 @@ def main(args):
 	bcf_file = args.bcf
 	ref_file = args.ref
 	bcf = ps.bcf(bcf_file)
-	bcf.generate_consensus(ref_file)
+	bcf.generate_consensus(ref_file,threads=args.threads)
 
 parser = argparse.ArgumentParser(description='TBProfiler pipeline',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('bcf',help='bcf file')
 parser.add_argument('ref',help='reference file')
+parser.add_argument('--threads','-t',default=4,type=int,help='reference file')
 parser.set_defaults(func=main)
 
 args = parser.parse_args()
