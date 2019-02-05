9e1a0113a141cbaac5fa1c337145408f6f8442a6
Jody Phelan <jody@Jodys-MacBook-Pro.local>
2019-02-05 11:49:12 +0000

    gemma

--- pathogenseq/gemma.py
+++ pathogenseq/gemma.py
@@ -6,37 +6,37 @@ from tqdm import tqdm
 import random
 r = random.SystemRandom()
 class ann:
-    annfile = ""
-    tabix = ""
-    def __init__(self,filename,tabix):
-        self.annfile = filename
-        self.tabix = tabix
-    def pos2ann(self,pos_tuple_list):
-        #pos_tuple_list = [("Chromosome",1),("Chromosome",2)]
-        if len(pos_tuple_list)<5000:
-            p1offset = -1
-            p2offset = 0
-            stype = "R"
-        else:
-            p1offset = 0
-            p2offset = 1
-            stype = "T"
+	annfile = ""
+	tabix = ""
+	def __init__(self,filename,tabix):
+		self.annfile = filename
+		self.tabix = tabix
+	def pos2ann(self,pos_tuple_list):
+		#pos_tuple_list = [("Chromosome",1),("Chromosome",2)]
+		if len(pos_tuple_list)<5000:
+			p1offset = -1
+			p2offset = 0
+			stype = "R"
+		else:
+			p1offset = 0
+			p2offset = 1
+			stype = "T"
 
-        num = r.randint(1,1000000)+r.randint(1,1000000)
-        temp_bed_file = "temp.%s.bed" % (num)
-        OUT = open(temp_bed_file,"w")
-        for chrom,pos in pos_tuple_list:
-            OUT.write("%s\t%s\t%s\n" % (chrom,int(pos)+p1offset,int(pos)+p2offset))
-        OUT.close()
-        results = defaultdict(dict)
-        for l in subprocess.Popen("%s %s -%s %s " % (self.tabix,self.annfile,stype,temp_bed_file),stdout=subprocess.PIPE,shell=True).stdout:
-            # added .decode() to convert binary output of subprocess.Popen to string
-            l = l.decode()
-            arr = l.rstrip().split()
-            results[arr[0]][int(arr[1])] = {"change_pos":arr[7],"ref_nt":arr[2],"ref_codon":arr[6],"ref_aa":arr[11],"chr":arr[0],"pos":int(arr[1]),"rv":arr[15],"gene":arr[16],"gene_syn":arr[17],"ncr":arr[18],"start":arr[19],"end":arr[20],"strand":arr[21],"codon_num":arr[24],"gene_nt":arr[25],"operon":arr[26]}
+		num = r.randint(1,1000000)+r.randint(1,1000000)
+		temp_bed_file = "temp.%s.bed" % (num)
+		OUT = open(temp_bed_file,"w")
+		for chrom,pos in pos_tuple_list:
+			OUT.write("%s\t%s\t%s\n" % (chrom,int(pos)+p1offset,int(pos)+p2offset))
+		OUT.close()
+		results = defaultdict(dict)
+		for l in subprocess.Popen("%s %s -%s %s " % (self.tabix,self.annfile,stype,temp_bed_file),stdout=subprocess.PIPE,shell=True).stdout:
+			# added .decode() to convert binary output of subprocess.Popen to string
+			l = l.decode()
+			arr = l.rstrip().split()
+			results[arr[0]][int(arr[1])] = {"alt_aa":{arr[3]:arr[12],arr[4]:arr[13],arr[5]:arr[14]},"change_pos":arr[7],"ref_nt":arr[2],"ref_codon":arr[6],"ref_aa":arr[11],"chr":arr[0],"pos":int(arr[1]),"rv":arr[15],"gene":arr[16],"gene_syn":arr[17],"ncr":arr[18],"start":arr[19],"end":arr[20],"strand":arr[21],"codon_num":arr[24],"gene_nt":arr[25],"operon":arr[26]}
 
-        os.remove(temp_bed_file)
-        return results
+		os.remove(temp_bed_file)
+		return results
 
 
 class gemma_results:
@@ -77,6 +77,11 @@ class gemma_results:
 		tmp.add_results([x for x in self.data if x["gene"] in genes])
 		return tmp
 	def create_tb_panel(self,drug,ann_file,cutoff=1e-5):
+		amino_acids = ['Cys', 'Ile', 'Ser', 'Val', 'Gly', 'Gln', 'Pro', 'Lys', 'Stop', 'Thr', 'Phe', 'Ala', 'Met', 'Asp', 'His', 'Leu', 'Arg', 'Trp', 'Glu', 'Asn', 'Tyr']
+		aa_long2short = {"Ala":"A","Arg":"R","Asn":"N","Asp":"D","Cys":"C","Gln":"Q","Glu":"E","Gly":"G","His":"H","Ile":"I","Leu":"L","Lys":"K","Met":"M","Phe":"F","Pro":"P","Ser":"S","Thr":"T","Trp":"W","Tyr":"Y","Val":"V","Stop":"*", "-":"-"}
+		aa_short2long = {'A': 'Ala', 'R': 'Arg', 'N': 'Asn', 'D': 'Asp', 'C': 'Cys', 'Q': 'Gln', 'E': 'Glu', 'G': 'Gly', 'H': 'His', 'I': 'Ile', 'L': 'Leu', 'K': 'Lys', 'M': 'Met', 'F': 'Phe', 'P': 'Pro', 'S': 'Ser', 'T': 'Thr', 'W': 'Trp', 'Y': 'Tyr', 'V': 'Val', '*': 'Stop', '-': '-'}
+
+
 		target_genes = {
 		"amikacin":["rrs"],
 		"bedaquiline":["Rv0678"],
@@ -86,16 +91,20 @@ class gemma_results:
 		"ethionamide":["Rv3854c","Rv3855","Rv1482c-Rv1483","Rv1484"],
 		"fluoroquinolones":["Rv0005","Rv0006"],
 		"isoniazid":["Rv2428","Rv2427A-Rv2428","Rv1482c-Rv1483","Rv1484","Rv2245","Rv1908c","Rv1908c-Rv1909c"],
-		"kanamycin":["Rv2416c-Rv2417c","rss"],
+		"kanamycin":["Rv2416c-Rv2417c","rrs"],
 		"linezolid":["Rv0701","rrl"],
 		"para-aminosalicylic_acid":["Rv2447c","Rv2671","Rv2764c"],
 		"pyrazinamide":["Rv3601c","Rv2043c","Rv2043c-Rv2044c","Rv1630"],
 		"rifampicin":["Rv0667","Rv0668"],
 		"streptomycin":["Rv3919c","Rv0682","rrs"]}
+
 		self = self.cutoff_hits(cutoff)
 		self.tb_annotate_hits(ann_file)
 		self = self.restrict_hits_to_gene(target_genes[drug.lower()])
 		for d in self.data:
 			mut = ""
-			if
-			print("%s\t%s\t%s\t%s\t%s\t%s" % (drug,d["pos"],d["ref"],d["alt"],d["gene"]))
+			if "-" in d["gene"]:
+				 mut = "%s%s%s" % (d["ref"],d["annotation"]["gene_pos"],d["alt"])
+			else:
+				 mut = "%s%s%s" % (aa_short2long[d["annotation"]["ref_aa"]],d["annotation"]["codon_num"],aa_short2long[d["annotation"]["alt_aa"][d["alt"]]])
+			print("%s\t%s\t%s\t%s\t%s\t%s" % (drug,d["pos"],d["ref"],d["alt"],d["gene"],mut))
